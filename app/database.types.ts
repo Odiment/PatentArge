36e492455031206c30ce0cb626e879e7d1ec8f07
Odiment <odimentbilgi@gmail.com>
2023-10-27 11:17:29 +0300

    build hatalarını giderme 21

--- app/database.types.ts
+++ app/database.types.ts
@@ -43,114 +43,114 @@ export interface Database {
       }
       markalar: {
         Row: {
-          firma_id: string
-          firma_unvan: string
-          firma_ad: string
-          basvuru_no: string
-          id: string
+          firma_id: string | null
+          firma_unvan: string | null
+          firma_ad: string | null
+          basvuru_no: string | null
+          id: string | null
           created_at: string | null
           referans_no: string | null
           class_no: string | null
           marka_durumu: string | null
           basvuru_tarihi: string | null
-          logo_url: string
-          tp_logo_url: string
-          tp_avatar: string
-          marka: string
+          logo_url: string | null
+          tp_logo_url: string | null
+          tp_avatar: string | null
+          marka: string | null
           deger: string | null
           status: string | null
         }
         Insert: {
-          firma_id?: string
-          firma_unvan?: string
-          firma_ad?: string
-          basvuru_no?: string
-          id: string
+          firma_id?: string | null
+          firma_unvan?: string | null
+          firma_ad?: string | null
+          basvuru_no?: string | null
+          id: string | null
           created_at?: string | null
           referans_no?: string | null
           class_no?: string | null
           marka_durumu?: string | null
           basvuru_tarihi?: string | null
-          logo_url?: string
-          tp_logo_url?: string
-          tp_avatar?: string
-          marka?: string
+          logo_url?: string | null
+          tp_logo_url?: string | null
+          tp_avatar?: string | null
+          marka?: string | null
           deger?: string | null
           status?: string | null
         }
         Update: {
-          firma_id?: string
-          firma_unvan?: string
-          firma_ad?: string
-          basvuru_no?: string
-          id: string
+          firma_id?: string | null
+          firma_unvan?: string | null
+          firma_ad?: string | null
+          basvuru_no?: string | null
+          id: string | null
           created_at?: string | null
           referans_no?: string | null
           class_no?: string | null
           marka_durumu?: string | null
           basvuru_tarihi?: string | null
-          logo_url?: string
-          tp_logo_url?: string
-          tp_avatar?: string
-          marka?: string
+          logo_url?: string | null
+          tp_logo_url?: string | null
+          tp_avatar?: string | null
+          marka?: string | null
           deger?: string | null
           status?: string | null
         }
       }
       patentler: {
         Row: {
-          id: string
-          patent_title: string
-          firma_id: string
-          firma_unvan: string
-          firma_ad: string
+          id: string | null
+          patent_title: string | null
+          firma_id: string | null
+          firma_unvan: string | null
+          firma_ad: string | null
           created_at: string | null
-          basvuru_no: string
+          basvuru_no: string | null
           basvuru_tarihi: string | null
           referans_no: string | null
           class_no: string | null
           deger: string | null
           status: string | null
           patent_durumu: string | null
-          patent_figure_url: string
-          blob_patent_figure_url: string
-          product_figure_url: string
+          patent_figure_url: string | null
+          blob_patent_figure_url: string | null
+          product_figure_url: string | null
         }
         Insert: {
-          id: string
-          patent_title?: string
-          firma_id: string
-          firma_unvan?: string
-          firma_ad?: string
+          id: string | null
+          patent_title?: string | null
+          firma_id: string | null
+          firma_unvan?: string | null
+          firma_ad?: string | null
           created_at: string | null
-          basvuru_no?: string
+          basvuru_no?: string | null
           basvuru_tarihi?: string | null
           referans_no?: string | null
           class_no?: string | null
           deger?: string | null
           status?: string | null
           patent_durumu?: string | null
-          patent_figure_url?: string
-          blob_patent_figure_url?: string
-          product_figure_url?: string
+          patent_figure_url?: string | null
+          blob_patent_figure_url?: string | null
+          product_figure_url?: string | null
         }
         Update: {
-          id: string
-          patent_title?: string
-          firma_id: string
-          firma_unvan?: string
-          firma_ad?: string
+          id: string | null
+          patent_title?: string | null
+          firma_id: string | null
+          firma_unvan?: string | null
+          firma_ad?: string | null
           created_at: string | null
-          basvuru_no?: string
+          basvuru_no?: string | null
           basvuru_tarihi?: string | null
           referans_no?: string | null
           class_no?: string | null
           deger?: string | null
           status?: string | null
           patent_durumu?: string | null
-          patent_figure_url?: string
-          blob_patent_figure_url?: string
-          product_figure_url?: string
+          patent_figure_url?: string | null
+          blob_patent_figure_url?: string | null
+          product_figure_url?: string | null
         }
       }
     }
