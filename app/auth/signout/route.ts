150c1beb474864e2aff71d59bf95627a99fb9542
Fatih Yalinbas <odimentbilgi@gmail.com>
2023-11-01 21:07:01 +0300

    son denemeler

--- app/auth/signout/route.ts
+++ app/auth/signout/route.ts
@@ -2,26 +2,29 @@ import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { type NextRequest, NextResponse } from 'next/server'
 
-export const getSession = async () => {
+/* export const getSession = async () => {
     const cookieStore = cookies()
     const supabase = createRouteHandlerClient({ cookies: () => cookieStore })
 
     const {
         data: { session },
       } = await supabase.auth.getSession()
-    // Check if we have a session
       
 
       return {session, supabase}
 
-}
+} */
 
 export async function POST(req: NextRequest) {
   /* const supabase = createRouteHandlerClient({ cookies }) */
-  /* const cookieStore = cookies();
-  const supabase = createRouteHandlerClient({ cookies: () => cookieStore }); */
+   const cookieStore = cookies();
+  const supabase = createRouteHandlerClient({ cookies: () => cookieStore });
 
-  const {session, supabase} = await getSession()
+  /* const {session, supabase} = await getSession() */
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
 
   if (session) {
     await supabase.auth.signOut()} 
