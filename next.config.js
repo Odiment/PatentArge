487b4dc30d54bf2501b1732c163eb58d6eb38e96
Fatih Yalinbas <odimentbilgi@gmail.com>
2023-11-02 01:00:10 +0300

    next 13.5 en sade versiyon

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: [
-      'i.pinimg.com',
-      'iaxdtlsmlrqyczqwvzhk.supabase.co',
-      'qzxxwmyywwqvbreysvto.supabase.co',
-      'supabase.co',
-    ],
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
