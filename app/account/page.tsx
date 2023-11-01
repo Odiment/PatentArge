67353dea7d7ebbeade589dd854a7025061f3b06d
Fatih Yalinbas <odimentbilgi@gmail.com>
2023-11-01 21:55:45 +0300

    son denemeler

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,44 +1,77 @@
-import { cookies } from "next/headers"
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+import { cookies } from "next/headers";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 
 /* import { Database } from "../database.types" */
-import AccountForm from "./account-form"
+import AccountForm from "./account-form";
 
-import { redirect } from 'next/navigation'
+import { cache } from "react";
 
+import { Database } from "@/app/supabase";
 
-/* export const getSession = async () => {
-    const cookieStore = cookies()
-    const supabase = createServerComponentClient({ cookies: () => cookieStore })
+import { redirect } from "next/navigation";
+
+export const createServerSupabaseClient = cache(() => {
+  const cookieStore = cookies();
+  return createServerComponentClient<Database>({ cookies: () => cookieStore });
+});
+
+export const getSession = async () => {
+  const supabase = createServerSupabaseClient();
+  try {
     const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    const {
+      data: { session },
+    } = await supabase.auth.getSession();
+
+    if (!user) return null;
+
+    return session;
+  } catch (error) {
+    console.error("Error", error);
+    return null;
+  }
+};
+
+/* export const getUser = async () => {
+    const supabase = createServerSupabaseClient();
+    try {
+      const {
         data: { user },
       } = await supabase.auth.getUser();
-
+  
       const {
         data: { session },
-      } = await supabase.auth.getSession()
-
-      return {session}
-} */
+      } = await supabase.auth.getSession();
+  
+      if (!user) return null;
+  
+      return user;
+    } catch (error) {
+      console.error("Error", error);
+      return null;
+    }
+  }; */
+  
 
 export default async function Account() {
   /* const supabase = createServerComponentClient<Database>({ cookies }) */
-  const cookieStore = cookies();
-  const supabase = createServerComponentClient({ cookies: () => cookieStore });
 
-  const {
+ /*  const {
     data: { session },
-  } = await supabase.auth.getSession()
+  } = await supabase.auth.getSession(); */
 
-/*   const {session} = await getSession()
+  const session = await getSession()
 
   if (!session) {
     redirect('/')
-  } */
+  }
 
   return (
     <>
       <AccountForm session={session} />
     </>
-  )
+  );
 }
