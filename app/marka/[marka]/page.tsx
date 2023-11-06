adbd67a287834188fc5eee7c057cebcacf1ac960
Fatih Yalinbas <odimentbilgi@gmail.com>
2023-11-06 11:02:32 +0300

    marka arama kriterleri düzenlendi

--- app/marka/[marka]/page.tsx
+++ app/marka/[marka]/page.tsx
@@ -99,6 +99,8 @@ export default async function MarkaKart({ searchParams }: MarkaIdPageProps) {
 
   let items: MarkalarX[] | null = [];
   let markalarx: MarkalarX[] | null = [];
+  let arananFirmaMarka: MarkalarX[] | null = [];
+
   // admin yetkisinde tüm markaların görülebilmesi - erişilebilmesi
   if (profil != null) {
     if (profil[0].yetki !== "admin" && (firma != null || firma != undefined)) {
@@ -128,12 +130,19 @@ export default async function MarkaKart({ searchParams }: MarkaIdPageProps) {
       ) {
         result.push(thing);
       }
-
       return result;
     }, []);
 
-    /*   console.log("arananFirma");
-    console.log(arananFirma); */
+    let arananFirmaId = arananFirma?.map(({ firma_id }: any) => firma_id);
+
+    if (durumFirma === false) {
+      const { data: arananFirmaMarkax } = await supabase
+        .from("markalar")
+        .select()
+        .eq("firma_id", arananFirmaId[0]);
+
+      arananFirmaMarka = arananFirmaMarkax;
+    }
 
     var arananKategori = markalarx?.reduce((result: any, thing) => {
       if (
@@ -159,7 +168,21 @@ export default async function MarkaKart({ searchParams }: MarkaIdPageProps) {
       return result;
     }, []);
 
+    var arananFirmaVeKategori = markalarx?.reduce((result: any, thing) => {
+      if (thing.marka != null && thing.status != null) {
+        if (
+          thing.firma_unvan.includes(`${searchParams.firma}`) &&
+          thing.status.includes(`${searchParams.kategori}`)
+        ) {
+          result.push(thing);
+        }
+      }
+
+      return result;
+    }, []);
+
     let yalnizcaGecerli: MarkalarX[] = [];
+    let firmaYalnizcaGecerli: MarkalarX[] = [];
 
     let durumIptalOlmayan = searchParams.kategori;
 
@@ -172,6 +195,16 @@ export default async function MarkaKart({ searchParams }: MarkaIdPageProps) {
         }
         return result;
       }, []);
+      if (durumFirma === false) {
+        firmaYalnizcaGecerli = arananFirma?.reduce((result: any, thing: any) => {
+          if (thing.status != null) {
+            if (!thing.status.includes("iptal")) {
+              result.push(thing);
+            }
+          }
+          return result;
+        }, []);
+      }
 
       if (yalnizcaGecerli != null) {
         var arananYalnizcaGecerli = yalnizcaGecerli.reduce(
@@ -186,44 +219,117 @@ export default async function MarkaKart({ searchParams }: MarkaIdPageProps) {
           },
           []
         );
+    }
+
+ if (firmaYalnizcaGecerli != null) {
+        var arananFirmaYalnizcaGecerli = firmaYalnizcaGecerli.reduce(
+          (result: any, thing) => {
+            if (thing.marka != null) {
+              if (thing.firma_unvan.includes(`${searchParams.firma}`)) {
+                result.push(thing);
+              }
+            }
+
+            return result;
+          },
+          []
+        );
+        }
+    }
+
+    var firmadaAranan = arananFirma?.reduce((result: any, thing: any) => {
+        if (thing.marka != null && thing.marka.includes(`${searchParams.name}`)) {
+          result.push(thing);
+        }
+  
+        return result;
+      }, []);
+  
+      var firmadaArananKategori = arananFirma?.reduce((result: any, thing: any) => {
+        if (
+          thing.status != null &&
+          thing.status.includes(`${searchParams.kategori}`)
+        ) {
+          result.push(thing);
+        }
+  
+        return result;
+      }, []);
+
+    // Eğer firma araması yapılmadıysa, standart filtreleme yap...
+    if (durumFirma === true) {
+      if (durum === true) {
+        items = markalarx;
+      } else items = aranan;
+
+      if (durumKategori === true) {
+        items = markalarx;
+      } else items = arananKategori;
+
+      if (durum === false && durumKategori === false) {
+        items = arananVeKategori;
+      }
+
+      if (searchParams.kategori === "tumu") {
+        items = markalarx;
+      }
+
+      let durumTumuVeAranan =
+        searchParams.kategori === "tumu" && durum === false;
+
+      if (durumTumuVeAranan === true) {
+        items = aranan;
+      }
+
+      if (searchParams.kategori === "yalnizcaGecerli") {
+        items = yalnizcaGecerli;
+      }
+
+      let durumYalnizcaGecerliVeAranan =
+        searchParams.kategori === "yalnizcaGecerli" && durum === false;
+
+      if (durumYalnizcaGecerliVeAranan === true) {
+        items = arananYalnizcaGecerli;
       }
     }
 
-    if (durumFirma === true) {
-      items = markalarx;
-    } else items = arananFirma;
+   
 
-    if (durum === true) {
-      items = markalarx;
-    } else items = aranan;
+    // Eğer firma bazlı arama yapılıyorsa önceliği firma aramasına ver...
+    if (durumFirma === false) {
+      if (durum === true) {
+        items = arananFirma;
+      } else items = firmadaAranan;
 
-    if (durumKategori === true) {
-      items = markalarx;
-    } else items = arananKategori;
+      if (durumKategori === true) {
+        items = arananFirma;
+      } else items = firmadaArananKategori;
 
-    if (durum === false && durumKategori === false) {
-      items = arananVeKategori;
-    }
+      if (durum === false && durumKategori === false) {
+        items = arananFirmaVeKategori;
+      }
 
-    if (searchParams.kategori === "tumu") {
-      items = markalarx;
-    }
+      if (searchParams.kategori === "tumu") {
+        items = arananFirma;
+      }
 
-    let durumTumuVeAranan = searchParams.kategori === "tumu" && durum === false;
+      let durumTumuVeAranan =
+        searchParams.kategori === "tumu" && durum === false;
 
-    if (durumTumuVeAranan === true) {
-      items = aranan;
-    }
+      if (durumTumuVeAranan === true) {
+        items = arananFirma;
+      }
 
-    if (searchParams.kategori === "yalnizcaGecerli") {
-      items = yalnizcaGecerli;
-    }
+      if (searchParams.kategori === "yalnizcaGecerli") {
+        items = firmaYalnizcaGecerli;
+      }
 
-    let durumYalnizcaGecerliVeAranan =
-      searchParams.kategori === "yalnizcaGecerli" && durum === false;
+      let durumYalnizcaGecerliVeAranan =
+        searchParams.kategori === "yalnizcaGecerli" && durum === false;
 
-    if (durumYalnizcaGecerliVeAranan === true) {
-      items = arananYalnizcaGecerli;
+      if (durumYalnizcaGecerliVeAranan === true) {
+        items = arananFirmaYalnizcaGecerli;
+      }
     }
   }
 
@@ -239,9 +345,8 @@ export default async function MarkaKart({ searchParams }: MarkaIdPageProps) {
   ) as React.Key | null | undefined;
 
   return (
-    <div
-      className="flex flex-col gap-y-8 pt-5 object-contain ml-[7px] md:ml-[55px] lg:ml-[115px] mr-[10px]">
-      {/* {profil != null && (
+    <div className="flex flex-col gap-y-8 pt-5 object-contain ml-[7px] md:ml-[55px] lg:ml-[115px] mr-[10px]">
+      {profil != null && (
         <div key={profil[0].yetki} className="grid grid-cols-8 gap-4">
           <div key={basvuru_no} className="col-span-4">
             <Filter />
@@ -252,7 +357,7 @@ export default async function MarkaKart({ searchParams }: MarkaIdPageProps) {
             </div>
           )}
         </div>
-      )} */}
+      )}
 
       <MarkaList items={items} bilgiler={items} userid={user?.id!} />
     </div>
